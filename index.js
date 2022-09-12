4c44a53d9a038699bcbe41471992ea6e49ee2952
Happiness-code <happinessjusticeebomah@gmail.com>
2022-09-12 11:47:20 +0200

    my index.html

--- index.js
+++ index.js
@@ -1,1 +1,80 @@
-alert("hello")
+function displayWeather(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(city) {
+  let apiKey = "d33243fa11c3284dcffcf337fc75caaa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function showCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+search("Madrid,Spain");
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", showCity);
+function searchLocation(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "d33243fa11c3284dcffcf337fc75caaa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&`;
+  axios.get(apiUrl).then(displayWeather);
+}
+function locationButton(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let currentButton = document.querySelector("#current-button");
+currentButton.addEventListener("click", locationButton);
+let date = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thurday",
+  "Friday",
+  "Saturday",
+];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let year = date.getFullYear();
+let day = days[date.getDay()];
+let month = months[date.getMonth()];
+let formateDate = date.getDate();
+let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let h3 = document.querySelector("h3");
+h3.innerHTML = `${day}, ${month} ${formateDate}, ${year}. ${hours}:${minutes}`;
