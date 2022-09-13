2ac714fc1072fec8c59fadcd06efe66c9326299e
Happiness-code <happinessjusticeebomah@gmail.com>
2022-09-14 01:25:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#temperature").innerHTML = Math.round( response.data.main.temp
     );
-  celiusTemperature = response.data.main.temp
+  celiusTemperature = response.data.main.temp;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
