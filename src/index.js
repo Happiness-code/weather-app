fec1e9343094b536bd053d11a3adcacd3d5353b2
Happiness-code <happinessjusticeebomah@gmail.com>
2022-09-14 13:43:33 +0200

    added to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function displayWeather(response) {
-  console.log(response.data);
+
   document.querySelector("#city").innerHTML = response.data.name;
+   celsiusTemperature = response.data.main.temp;
     document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
@@ -28,7 +29,31 @@ function showCity(event) {
   let city = document.querySelector("#city-input").value;
   search(city);
 }
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let celsiusTemperature = null;
 search("Madrid, Spain");
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", showCity);
 function searchLocation(position) {
