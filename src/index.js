56b44149c344647c44080cc1db70e5e7eb334008
Happiness-code <happinessjusticeebomah@gmail.com>
2022-09-18 13:56:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ function search(city) {
 
 function getForecast(coordinates) {
   let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
-  let apiUrl = ` https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&&appid=${apiKey}&units=matric`;
+  let apiUrl = ` https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -45,12 +45,18 @@ function displayForecast(response) {
               <div class="WeatherForecastPreview">
                 <div class="forecast-time">${formatDay(forecastDay.dt)}</div>
           <img
-                  src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""
                   width="42">
           <div class="forecast-temperature">
-            <span class="forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
-            <span class="forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
+            <span class="forecast-temperature-max">${Math.round(
+              forecastDay.temp.max
+            )}°</span>
+            <span class="forecast-temperature-min">${Math.round(
+              forecastDay.temp.min
+            )}°</span>
           </div>
           </div>
         </div>`;
